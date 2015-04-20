c322cc92566203a84553374d7805ce497b9f8c15
Roy Olav Purser <admin@royolav.net>
2015-04-20 14:58:43 +0200

    api

--- wol.go
+++ wol.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"bytes"
-	"encoding/binary"
 	"fmt"
 	"net"
+	"bufio"
+	"strings"
+	"encoding/json"
+	"encoding/binary"
+	"io/ioutil"
+	"errors"
 )
 
 type MACAddress [6]byte
@@ -17,15 +22,16 @@ type MagicPacket struct {
     payload [16]MACAddress
 }
 
+
 func main(){
-    sendPacket();
+    httpServer();
 }
 
 // This function accepts a MAC Address string, and returns a pointer to
 // a MagicPacket object. A Magic Packet is a broadcast frame which
 // contains 6 bytes of 0xFF followed by 16 repetitions of a given mac address.
 
-func NewMagicPacket(mac string) (*MagicPacket, error) {
+func makePacket(mac string) (*MagicPacket, error) {
     var packet MagicPacket
     var macAddr MACAddress
 
@@ -53,28 +59,113 @@ func NewMagicPacket(mac string) (*MagicPacket, error) {
     return &packet, nil
 }
 
-func sendPacket(){
-    packet,_ := NewMagicPacket("48:5b:39:20:25:2c");
+func getHost(host string)(string,error){
+
+    file, err := ioutil.ReadFile("hosts.json");
+    hosts := make(map[string]string);
     
-    var buf bytes.Buffer
-    
-    binary.Write(&buf, binary.BigEndian, packet)
-    
-    remoteAddr,_ := net.ResolveUDPAddr("udp", "255.255.255.255:9")
-    
-    connection, err := net.DialUDP("udp", nil, remoteAddr)
-    
-    var bytesWritten int;
-    
-    if(err != nil){
-        fmt.Println("connection failed", err)
-    }else{
-        bytesWritten, err = connection.Write(buf.Bytes());
-        if(err != nil){
-            fmt.Println("packet not sendt");
+    if(err == nil){
+        err = json.Unmarshal(file, &hosts);
+        if((err == nil)  && (len(hosts[host]) > 0)){
+            
+            return hosts[host], nil;
         }
-    
     }
-    fmt.Println("Bytes written: ", bytesWritten)
+    return "", errors.New("host not found" + host);
+    
+    
+}
+
+
+func sendPacket(apiData map[string]string)(error){
+
+    host,err := getHost(apiData["host"])
+    if(err == nil){
+
+        packet,_ := makePacket(host);
+    
+        var buf bytes.Buffer
+    
+        binary.Write(&buf, binary.BigEndian, packet)
+    
+        remoteAddr,_ := net.ResolveUDPAddr("udp", "255.255.255.255:9")
+    
+        connection, err := net.DialUDP("udp", nil, remoteAddr)
+    
+        if(err != nil){
+            return err
+        }else{
+            _,err := connection.Write(buf.Bytes());
+            if(err == nil){
+                return nil
+            }else{
+                return err
+            }
+        }
+    }
+    return err
+}
+
+func httpServer(){
+    ln, _ := net.Listen("tcp", ":8081")
+    
+    for{
+        conn,_ := ln.Accept()
+        go parseHttp(conn)
+    }
+
+}
+
+func parseHeader(text bufio.Reader)(map[string][]string){
+
+    str := "init"
+
+    header := make(map[string][]string);
+
+    for(len(str) > 3){
+    
+        str,_ = text.ReadString('\n');
+        
+        val := strings.Split(strings.TrimSpace(str), " ");
+        
+        header[val[0]] = val[1:]
+        }
+    return header;
+}
+
+func parseHttp(conn net.Conn){
+
+    message := bufio.NewReader(conn);
+    
+    header := parseHeader(*message);
+    
+    reply := []byte("HTTP/1.1 200 OK\nAccess-Control-Allow-Origin: *\nAccess-Control-Allow-Headers: x-api\nAccess-Control-Allow-Methods: POST\nContent-Type: text/html; charset=utf-8\n\n");
+    
+    var status string;  
+    
+    conn.Write(reply);
+    
+    if(len(header["x-api:"]) > 0){
+    
+        jsonString := []byte(header["x-api:"][0])
+    
+        in := make(map[string]string);
+
+        err := json.Unmarshal((jsonString), &in);
+        
+        fmt.Println(in);
+        
+        if(err == nil){
+            if(sendPacket(in) == nil){
+                status = "ok";
+            }else{
+                status = "fail";
+            }
+        }
+    }
+    
+    conn.Write([]byte(status))
+    
+    conn.Close();
 }
 
