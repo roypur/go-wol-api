c2d43aef171a22046f6408b3a140a33c7e37910c
Roy Olav Purser <admin@royolav.net>
2015-04-21 15:57:39 +0200

    edit user

--- wol.go
+++ wol.go
@@ -29,10 +29,8 @@ type MagicPacket struct {
 
 func main(){
     
-    if(os.Args[1] == "add"){
-        modUser("add");
-    }else if(os.Args[1] == "edit"){
-        modUser("edit");
+    if((os.Args[1] == "add") || (os.Args[1] == "edit") || (os.Args[1] == "delete")){
+        modUser(os.Args[1]);
     }else if(os.Args[1] == "server"){
         httpServer();
     };
@@ -41,6 +39,18 @@ func main(){
 // This function accepts a MAC Address string, and returns a pointer to
 // a MagicPacket object. A Magic Packet is a broadcast frame which
 // contains 6 bytes of 0xFF followed by 16 repetitions of a given mac address.
+
+func httpServer(){
+    ln, _ := net.Listen("tcp", ":8081")
+    
+    for{
+        conn,_ := ln.Accept()
+        go parseHttp(conn)
+    }
+
+}
+
+
 
 func makePacket(mac string) (*MagicPacket, error) {
     var packet MagicPacket
@@ -107,8 +117,13 @@ func isUser(user string, pass string)(error){
  
 }
 
-func modUser(operation string)(error){
-    var pass string = url.QueryEscape(string(os.Args[3]));
+func modUser(operation string){
+    var pass string;
+    
+    if(operation!="delete"){
+        pass = url.QueryEscape(string(os.Args[3]));
+    };
+    
     var user string = url.QueryEscape(string(os.Args[2]));
     
     
@@ -118,41 +133,50 @@ func modUser(operation string)(error){
     if(err == nil){
         err = json.Unmarshal(file, &users);
         if(err == nil){
-            
-            if(len(users[user]) == 0 || operation == "edit"){
-            
-            
-                hash, err := bcrypt.GenerateFromPassword([]byte(pass), 10)
-            
-                if(err==nil){
-                    users[user] = string(hash);
-            
-                    b, err := json.MarshalIndent(users,"","    ");
-                
-                    if(err==nil){
-                
-                        f, err := os.Create("users.json")
-    
-                        if(err != nil){
-                            fmt.Println(err);
-                        }else{
         
-                            _,err := io.WriteString(f, string(b));
-                            
-                            fmt.Println(err);
-                        }   
-                
+            var hash []byte;
             
+            if(operation=="delete"){
+                delete(users,user);
+            }else if(len(users[user]) == 0){
+                hash, err = bcrypt.GenerateFromPassword([]byte(pass), 10);
+                users[user] = string(hash);
+            }else{
+                err = errors.New("User exists")
+            }
+                
+            if(err==nil){
+                    
+                if(operation=="delete"){
+                    delete(users,user);
+                }else{
+                    users[user] = string(hash);
+                }
+                    
+                b, err := json.MarshalIndent(users,"","    ");
+                
+                if(err==nil){
+                
+                    f, err := os.Create("users.json")
+
+                    if(err != nil){
+                        fmt.Println(err);
+                    }else{
+                        _,err := io.WriteString(f, string(b));
+                        
                         if(err == nil){
-                            fmt.Println(string(hash));
-                            return nil;
+                            
+                            var grammar string = strings.Replace(operation + "ed","ee","e",-1);
+                            
+                            fmt.Println("user<" + user + "> " + grammar);
                         }
                     }
                 }
+            }else{
+                fmt.Println(err);
             }
         }
     }
-    return errors.New("something went wrong");
 }
 
 func sendPacket(apiData map[string]string)(error){
@@ -184,33 +208,6 @@ func sendPacket(apiData map[string]string)(error){
     return err
 }
 
-func httpServer(){
-    ln, _ := net.Listen("tcp", ":8081")
-    
-    for{
-        conn,_ := ln.Accept()
-        go parseHttp(conn)
-    }
-
-}
-
-func parseHeader(text bufio.Reader)(map[string][]string){
-
-    str := "init"
-
-    header := make(map[string][]string);
-
-    for(len(str) > 3){
-    
-        str,_ = text.ReadString('\n');
-        
-        val := strings.Split(strings.TrimSpace(str), " ");
-        
-        header[val[0]] = val[1:]
-        }
-    return header;
-}
-
 func parseHttp(conn net.Conn){
 
     message := bufio.NewReader(conn);
@@ -228,8 +225,6 @@ func parseHttp(conn net.Conn){
         jsonString := []byte(header["x-api:"][0])
     
         in := make(map[string]string);
-
-
 
         err := json.Unmarshal((jsonString), &in);
         
@@ -254,3 +249,19 @@ func parseHttp(conn net.Conn){
     conn.Close();
 }
 
+func parseHeader(text bufio.Reader)(map[string][]string){
+
+    str := "init"
+
+    header := make(map[string][]string);
+
+    for(len(str) > 3){
+    
+        str,_ = text.ReadString('\n');
+        
+        val := strings.Split(strings.TrimSpace(str), " ");
+        
+        header[val[0]] = val[1:]
+        }
+    return header;
+}
