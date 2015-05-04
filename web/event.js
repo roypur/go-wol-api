8a1fb5ec997676aeaa4447f7f7b14ddcdb431e2e
Roy Olav Purser <admin@royolav.net>
2015-05-04 18:42:00 +0200

    merge js

--- web/event.js
+++ web/event.js
@@ -42,3 +42,18 @@ function serverDown()
     $("#outer").css({"top":"80px"});
     $("#error").html("Service Unavailable"); 
 }
+
+function wake(host, user, pass, handle)
+{
+
+    host = f.encode(host);
+    
+    user = f.encode(user);
+    
+    pass = f.encode(pass);
+
+    var data = {"host":host,"user":user,"pass":pass}
+    var apiData = JSON.stringify(data);
+
+    f.api(apiData, "http://home.purser.it:8781", handle);
+}
