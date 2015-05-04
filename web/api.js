bddd07e393fb784916f96d32f2f530589c561a1f
Roy Olav Purser <admin@royolav.net>
2015-05-04 13:37:10 +0200

    lib

--- web/api.js
+++ web/api.js
@@ -1,37 +1,14 @@
 function wake(host, user, pass, handle)
 {
 
-    host = goEncode(host);
+    host = f.encode(host);
     
-    user = goEncode(user);
+    user = f.encode(user);
     
-    pass = goEncode(pass);
+    pass = f.encode(pass);
 
     var data = {"host":host,"user":user,"pass":pass}
     var apiData = JSON.stringify(data);
 
-    $.ajax(
-    {
-        url: "http://home.purser.it:8781",
-        type: "POST",
-        headers: {'x-api':apiData},
-        success: handle
-    });
+    f.api(apiData, "http://home.purser.it:8781", handle);
 }
-
-function goEncode(str)
-{
-    str = str.replace('-', '-a');    
-    str = str.replace('[', '-b');
-    str = str.replace(']', '-c');
-    str = str.replace('{', '-d');
-    str = str.replace('}', '-e');
-    str = str.replace("'", '-f');
-    str = str.replace('"', '-g');
-    str = str.replace(' ', '-h');
-    str = str.replace(',', '-i');
-    str = str.replace(':', '-j');
-    
-    return str
-}
-
